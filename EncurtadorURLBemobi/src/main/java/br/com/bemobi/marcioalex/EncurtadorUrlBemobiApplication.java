b4db5b618b88bec6af3715b22d4df8ddc95ecf52
marcioalexfig <alex.marcio@gmail.com>
2017-10-19 12:32:11 -0200

    Primeiro commit

---
+++ EncurtadorURLBemobi/src/main/java/br/com/bemobi/marcioalex/EncurtadorUrlBemobiApplication.java
@@ -0,0 +1,12 @@
+package br.com.bemobi.marcioalex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUrlBemobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUrlBemobiApplication.class, args);
+	}
+}
