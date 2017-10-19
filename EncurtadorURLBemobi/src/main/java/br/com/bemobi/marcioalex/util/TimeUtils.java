cdb9c15594b7aee18e3c8eaf110a19facd7e2a9e
marcioalexfig <alex.marcio@gmail.com>
2017-10-19 13:23:07 -0200

    Inclusão de swagger para documentar e servir de interface para a API, configuração de uma classe Response com alguns campos de retorno da API e uma classe TimeUtils para calculo de tempo de resposta das requisições

---
+++ EncurtadorURLBemobi/src/main/java/br/com/bemobi/marcioalex/util/TimeUtils.java
@@ -0,0 +1,9 @@
+package br.com.bemobi.marcioalex.util;
+
+public class TimeUtils {
+
+	public static Long tempoTotal (Long tempoInicial, Long tempoFinal) {
+		return (tempoFinal - tempoInicial);
+	}
+	
+}
